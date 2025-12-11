f9042a981dc71179013d9ac08081cfe52f9748b6
MEDWED <egor2239@gmail.com>
2025-12-11 23:30:52 +0300

    added

---
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -0,0 +1,17 @@
+package jm.task.core.jdbc.util;
+import java.sql.*;
+import java.util.*;
+
+public class Util {
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "123123";
+    private static final String URL = "jdbc:mysql://localhost:3306/lesson";
+
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            throw new RuntimeException("Ошибка");
+        }
+    }
+}

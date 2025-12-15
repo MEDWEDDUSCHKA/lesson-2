1fbe32164dbb8c864f190d5e111bfd17375af5f5
MEDWED <egor2239@gmail.com>
2025-12-15 10:25:36 +0300

    fixed the errors

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,32 +1,29 @@
 package jm.task.core.jdbc.dao;
 
-import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
+import jm.task.core.jdbc.model.User;
 
 import java.sql.*;
 import java.util.*;
 
 public class UserDaoJDBCImpl implements UserDao {
-    private static final String create = "CREATE TABLE IF NOT EXISTS users (" +
+    private static final String CREATE_TABEL_SQL = "CREATE TABLE IF NOT EXISTS users (" +
             "id BIGINT AUTO_INCREMENT PRIMARY KEY, " +
             "name VARCHAR(50), " +
             "last_name VARCHAR(50), " +
             "age TINYINT" +
             ")";
-    private static final String drop = "DROP TABLE IF EXISTS users";
-    private static final String insert = "INSERT INTO users (name, last_name, age) VALUES (?, ?, ?)";
-    private static final String deleteUser = "DELETE FROM users WHERE id = ?";
-    private static final String select = "SELECT id, name, last_name, age FROM users";
-    private static final String clean = "DELETE FROM users";
+    private static final String DROP_TABEL_SQL= "DROP TABLE IF EXISTS users";
+    private static final String INSERT_USER_SQL = "INSERT INTO users (name, last_name, age) VALUES (?, ?, ?)";
+    private static final String DELETE_FROM_USER_SQL = "DELETE FROM users WHERE id = ?";
+    private static final String SELECT_USERS_SQL = "SELECT id, name, last_name, age FROM users";
+    private static final String CLEAN_TABLE_SQL = "DELETE FROM users";
 
-    private final Connection conn;
+    private final Connection conn = new Util().getConnection();
 
-    public UserDaoJDBCImpl() {
-            this.conn = Util.getConnection();
-    }
 
     public void createUsersTable() {
-        try (PreparedStatement stmt = conn.prepareStatement(create)) {
+        try (PreparedStatement stmt = conn.prepareStatement(CREATE_TABEL_SQL)) {
             stmt.executeUpdate();
         } catch (SQLException e) {
             throw new RuntimeException("Ошибка при созданий", e);
@@ -34,7 +31,7 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void dropUsersTable() {
-        try (PreparedStatement stmt = conn.prepareStatement(drop)) {
+        try (PreparedStatement stmt = conn.prepareStatement(DROP_TABEL_SQL)) {
             stmt.executeUpdate();
         } catch (SQLException e) {
             throw new RuntimeException("Ошибка дропа", e);
@@ -42,7 +39,7 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        try (PreparedStatement stmt = conn.prepareStatement(insert)) {
+        try (PreparedStatement stmt = conn.prepareStatement(INSERT_USER_SQL)) {
             stmt.setString(1, name);
             stmt.setString(2, lastName);
             stmt.setByte(3, age);
@@ -53,7 +50,7 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void removeUserById(long id) {
-        try (PreparedStatement stmt = conn.prepareStatement(deleteUser)) {
+        try (PreparedStatement stmt = conn.prepareStatement(DELETE_FROM_USER_SQL)) {
             stmt.setLong(1, id);
             stmt.executeUpdate();
         } catch (SQLException e) {
@@ -64,7 +61,7 @@ public class UserDaoJDBCImpl implements UserDao {
     public List<User> getAllUsers() {
         List<User> users = new ArrayList<>();
 
-        try (PreparedStatement stmt = conn.prepareStatement(select);
+        try (PreparedStatement stmt = conn.prepareStatement(SELECT_USERS_SQL);
              ResultSet rs = stmt.executeQuery()) {
             while (rs.next()) {
                 User user = new User();
@@ -83,14 +80,10 @@ public class UserDaoJDBCImpl implements UserDao {
     }
 
     public void cleanUsersTable() {
-        try (PreparedStatement stmt = conn.prepareStatement(clean)) {
+        try (PreparedStatement stmt = conn.prepareStatement(CLEAN_TABLE_SQL)) {
             stmt.executeUpdate();
         } catch (SQLException e) {
             throw new RuntimeException("Ошибка очистки", e);
         }
-    }
-
-    public void closeConnection() throws SQLException {
-        conn.close();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -7,7 +7,7 @@ public class Util {
     private static final String PASSWORD = "123123";
     private static final String URL = "jdbc:mysql://localhost:3306/lesson";
 
-    public static Connection getConnection() {
+    public Connection getConnection() {
         try {
             return DriverManager.getConnection(URL, USERNAME, PASSWORD);
         } catch (SQLException e) {
